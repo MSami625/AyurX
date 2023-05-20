c5b41f284d700018a985819950b321f44e3b97cf
Shubham <Shubham>
2023-05-20 13:48:50 +0530

    s3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let wave1 = document.getElementById('wave1');
 let wave2 = document.getElementById('wave2');
 let wave3 = document.getElementById('wave3');
 let wave4 = document.getElementById('wave4');
+let text = document.getElementById('text_main');
 
 
 window.addEventListener('scroll',function(){
@@ -11,6 +12,7 @@ window.addEventListener('scroll',function(){
     wave2.style.backgroundPositionX = 300 + value * -4 + 'px';
     wave3.style.backgroundPositionX = 200 + value * 2 + 'px';
     wave4.style.backgroundPositionX = 100 + value * -2 + 'px';
+    text.style.marginLeft= value * 2.5 + 'px';
     
 
 })
